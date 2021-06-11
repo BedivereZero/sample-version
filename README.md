a6815e5185ddf1741b3e0964b49432bd86f0b3ae
BedivereZero <BedivereZero@gmail.com>
2021-06-11 14:07:07 +0800

    Add build and clean scripts
    Signed-off-by: BedivereZero <BedivereZero@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # sample-version
+
+- [x] 版本信息
+  - [x] 语义版本号
+  - [x] gitVersion
+  - [x] git commit sha
+  - [x] 与上一个标签的距离
+  - [ ] go 版本
+  - [ ] 编译器
+  - [ ] 平台
+- [ ] Version 包
+- [x] 构建时间
+- [x] git tree 是否干净
+- [ ] 构建过程中生成版本信息
+- [ ] 使用脚本或 Makefile 实现
+- [ ] 什么是 `ldflags`
--- build/lib/version.sh
+++ build/lib/version.sh
@@ -4,9 +4,9 @@ sample::lib::version::get_version_vars() {
 	SAMPLE_GIT_COMMIT="$(git rev-parse HEAD 2>/dev/null)"
 
 	if git_status="$(git status --porcelain 2>/dev/null)" && [[ -z "${git_status}" ]]; then
-		SAMPLE_GIT_TREE_STATE="clean"
+		GIT_TREE_STATE="clean"
 	else
-		SAMPLE_GIT_TREE_STATE="dirty"
+		GIT_TREE_STATE="dirty"
 	fi
 
 	SAMPLE_GIT_VERSION="$(git describe --tags --match='v*' 2>/dev/null)"
@@ -14,7 +14,7 @@ sample::lib::version::get_version_vars() {
 	if [[ "${DASH_IN_VERSION}" == "---" ]]; then
 		SAMPLE_GIT_VERSION="$(echo "${SAMPLE_GIT_VERSION}" | sed "s/-\([0-9]\{1,\}\)-g\([0-9a-f]\{7\}\)$/.\1+\2/")"
 	fi
-	if [[ "${SAMPLE_GIT_TREE_STATE}" == "dirty" ]]; then
+	if [[ "${GIT_TREE_STATE}" == "dirty" ]]; then
 		SAMPLE_GIT_VERSION+="-dirty"
 	fi
 
