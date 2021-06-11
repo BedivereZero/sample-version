a07f47b4318d3e7cf079fa023aea47ef77c913e6
BedivereZero <BedivereZero@gmail.com>
2021-06-11 14:07:51 +0800

    Ignore output directory
    Signed-off-by: BedivereZero <BedivereZero@gmail.com>

--- build/lib/version.sh
+++ build/lib/version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sample::lib::version::get_version_vars() {
-	SAMPLE_GIT_COMMIT="$(git rev-parse HEAD 2>/dev/null)"
+	GIT_COMMIT="$(git rev-parse HEAD 2>/dev/null)"
 
 	if git_status="$(git status --porcelain 2>/dev/null)" && [[ -z "${git_status}" ]]; then
 		GIT_TREE_STATE="clean"
@@ -9,21 +9,21 @@ sample::lib::version::get_version_vars() {
 		GIT_TREE_STATE="dirty"
 	fi
 
-	SAMPLE_GIT_VERSION="$(git describe --tags --match='v*' 2>/dev/null)"
-	DASH_IN_VERSION="$(echo "${SAMPLE_GIT_VERSION}" | sed "s/[^-]//g")"
+	GIT_VERSION="$(git describe --tags --match='v*' 2>/dev/null)"
+	DASH_IN_VERSION="$(echo "${GIT_VERSION}" | sed "s/[^-]//g")"
 	if [[ "${DASH_IN_VERSION}" == "---" ]]; then
-		SAMPLE_GIT_VERSION="$(echo "${SAMPLE_GIT_VERSION}" | sed "s/-\([0-9]\{1,\}\)-g\([0-9a-f]\{7\}\)$/.\1+\2/")"
+		GIT_VERSION="$(echo "${GIT_VERSION}" | sed "s/-\([0-9]\{1,\}\)-g\([0-9a-f]\{7\}\)$/.\1+\2/")"
 	fi
 	if [[ "${GIT_TREE_STATE}" == "dirty" ]]; then
-		SAMPLE_GIT_VERSION+="-dirty"
+		GIT_VERSION+="-dirty"
 	fi
 
 	# major and minor
-	if [[ "${SAMPLE_GIT_VERSION}" =~ ^v([0-9]+)\.([0-9]+)(\.[0-9]+)?([-].*)?([+].*)?$ ]]; then
-		SAMPLE_GIT_MAJOR="${BASH_REMATCH[1]}"
-		SAMPLE_GIT_MINOR="${BASH_REMATCH[2]}"
+	if [[ "${GIT_VERSION}" =~ ^v([0-9]+)\.([0-9]+)(\.[0-9]+)?([-].*)?([+].*)?$ ]]; then
+		GIT_MAJOR="${BASH_REMATCH[1]}"
+		GIT_MINOR="${BASH_REMATCH[2]}"
 		if [[ -n "${BASH_REMATCH[4]}" ]]; then
-			SAMPLE_GIT_MINOR+="+"
+			GIT_MINOR+="+"
 		fi
 	fi
 }
